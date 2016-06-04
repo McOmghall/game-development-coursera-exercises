56ae992d3d839420b220676f3c1a5e3494283d46
McOmghall <pmgberm@gmail.com>
2016-06-04 18:37:11 +0200

    Added Small Changes to Solar System example project

--- Coursera Game Courses/Assets/Scripts/LookAtTarget.cs
+++ Coursera Game Courses/Assets/Scripts/LookAtTarget.cs
@@ -6,16 +6,19 @@ public class LookAtTarget : MonoBehaviour {
 	static public GameObject target; // the target that the camera should look at
 
 	void Start () {
-		if (target == null) 
+		if (LookAtTarget.target == null) 
 		{
-			target = this.gameObject;
+            LookAtTarget.target = this.gameObject;
 			Debug.Log ("LookAtTarget target not specified. Defaulting to parent GameObject");
 		}
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (target)
-			transform.LookAt(target.transform);
+		if (target) {
+           foreach (Camera camera in Camera.allCameras) {
+                camera.transform.LookAt(target.transform);
+            }
+        }
 	}
 }
