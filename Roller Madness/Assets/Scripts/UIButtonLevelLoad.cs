90781f3764798347db7d9743df8db703fc406758
McOmghall <pmgberm@gmail.com>
2016-06-06 20:46:30 +0200

    Started 3rd week project for Gamedev course

---
+++ Roller Madness/Assets/Scripts/UIButtonLevelLoad.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class UIButtonLevelLoad : MonoBehaviour {
+	
+	public string LevelToLoad;
+	
+	public void loadLevel() {
+        Debug.Log("Loading new level");
+        //Load the level from LevelToLoad
+        SceneManager.LoadScene(LevelToLoad);
+	}
+}
